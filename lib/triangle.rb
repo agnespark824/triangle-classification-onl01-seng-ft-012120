07876a2dc5d475d158b3425c017badad642a6156
Agnes Park <agnespark824@gmail.com>
2020-02-09 06:36:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :length_one, :length_two, :length_three
+  
+  def initialization(length_one, length_two, length_three)
+    #@length_one = length_one
+    #@length_two = length_two
+    #@length_three = length_three
+    @array = [length_one, length_two, length_three]
+    binding.pry
+  end
+  
+  def kind
+    if @array.uniq.size == 3 && @array.all?{|n| n > 0} && @array.addition == true
+      :scalene
+    elsif @array.uniq.size == 2 && @array.all?{|n| n > 0} && @array.addition == true
+      :isosceles
+    elsif @array.uniq.size == 1 && @array.all?{|n| n > 0}
+      :equilateral
+    elsif @array.all?{|n| n > 0} == false || self.addition == false
+      raise TriangleError
+    end
+  end
+
+  def addition 
+    @array.permutation(3).all?{|a| a[0] + a[1] > a[2]}
+  end
+    
+
+
+  class TriangleError < StandardError
+  end
+
 end
+    
+    
